f85ff2eeb052a8a492689f0cc192afea5d0e69f0
jovencoda <genitor06@gmail.com>
2017-04-20 13:29:04 -0500

    Added provisioning script & django instalation

---
+++ provisioning/script.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# provisioning script that may be run several times
+# if LC_ALL is none, then set it, and also update /etc/environment
+
+LOCALE_FIX='LC_ALL=en_US.UTF-8'
+
+export $LOCALE_FIX
+echo $LOCALE_FIX >> /etc/environment
+
+
+apt-get update
+apt-get upgrade -y
+
+# apt-get install language-pack-UTF-8
+apt-get install -y emacs
+apt-get install -y python-virtualenv
+apt-get install -y python-dev
+apt-get install -y libpython-all-dev
+apt-get install -y libmysqlclient-dev
+apt-get install -y libjpeg-dev
+apt-get install -y libtiff5-dev
+apt-get install -y libjpeg8-dev
+apt-get install -y zlib1g-dev
+apt-get install -y libfreetype6-dev
+apt-get install -y liblcms2-dev
+apt-get install -y libwebp-dev
+apt-get install -y tcl8.6-dev
+apt-get install -y tk8.6-dev
+apt-get install -y python-tk
+apt-get install -y fish
+
+fish
+
+# Permissions
+chown -R vagrant:vagrant /var/www/html/
+
