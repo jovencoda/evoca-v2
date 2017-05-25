90c589b71bc1a241214ca78d9c2d151287f0c831
jovencoda <genitor06@gmail.com>
2017-05-24 23:17:44 -0500

    Small fix

--- evoca_v2/evoca_v2/settings.py
+++ evoca_v2/evoca_v2/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'django.contrib.gis',
-	'rest_framework',
-	'rest_framework.authtoken',
+    'django.contrib.gis',
+    'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
-	'core',
+    'core',
 ]
 
 MIDDLEWARE = [
