fdbe2e126c328e4a5d11f48d1783172bb71c91fb
jovencoda <genitor06@gmail.com>
2017-05-26 13:17:26 -0500

    Added auto run requirements on ssh login

--- provisioning/user_profile.sh
+++ provisioning/user_profile.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 if [ -n "$BASH_VERSION" ]; then
     source env/bin/activate
-	cd /var/www/html/evoca/
+    # Run requirements
+  pip install -r /var/www/html/evoca/evoca_v2/requirements.txt
+  cd /var/www/html/evoca/web-client
+  sudo npm install
+  # Back to main folder
+	cd /var/www/html/evoca
+
 	fish
 fi
