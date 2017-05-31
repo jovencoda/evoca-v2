5b09c9c598610cae21e7adc8a2d2fde13ff7d313
jovencoda <genitor06@gmail.com>
2017-05-30 23:45:57 -0500

    Joind dimension serializer with channel

--- evoca_v2/core/serializers.py
+++ evoca_v2/core/serializers.py
@@ -2,8 +2,13 @@
 from rest_framework import serializers as serial
 from core.models import *
 
-class ChannelSerializer(serial.ModelSerializer):
+class DimensionSerializer(serial.ModelSerializer):
+	class Meta:
+		model = Dimension
+		fields = ('id', 'uniqueID', 'name', 'slug' )
 
+class ChannelSerializer(serial.ModelSerializer):
+	dimensions = DimensionSerializer(many=True, read_only=True)
 	class Meta:
 		model = Channel
 		fields = ('name', 'about', 'dimensions', 'created_at', 'updated_at')
