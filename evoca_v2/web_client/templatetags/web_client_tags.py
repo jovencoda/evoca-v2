0cf9412c30392b0164ef1e7fcf858c5f81082807
jovencoda <genitor06@gmail.com>
2017-07-12 16:42:38 -0500

    Improvments to record list view

--- evoca_v2/web_client/templatetags/web_client_tags.py
+++ evoca_v2/web_client/templatetags/web_client_tags.py
@@ -1,5 +1,10 @@
 
 from django import template
+
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+from hashlib import md5
+
 from core.models import Channel
 
 register = template.Library()
@@ -12,3 +17,14 @@ def get_filtered_channels_list(active):
 def get_channels_list():
     print(Channel.objects.all())
     return Channel.objects.all()
+
+# Flavatar from @aleksig https://github.com/aleksisg/django-flavatar
+@register.filter(is_safe=True)
+@stringfilter
+def flavatar(value):
+    fistchar = value[:1].upper()
+    value = value.encode('utf-8')
+    hash = md5(value).hexdigest()
+    hash_values = (hash[:8], hash[8:16], hash[16:24])
+    number = tuple(int(value, 16)%256 for value in hash_values)
+    return mark_safe('<div class="label flavatar" style="background: rgb%s;">%s</div>' % (number, fistchar))
