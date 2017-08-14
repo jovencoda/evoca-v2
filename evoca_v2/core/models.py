3b14a96e8d5d581050835ac83d920a2ec01439e1
jovencoda <genitor06@gmail.com>
2017-08-14 12:46:00 -0500

    Improved API geo metadata

--- evoca_v2/core/models.py
+++ evoca_v2/core/models.py
@@ -62,29 +62,38 @@ class Record(TimeBot):
 	country = models.CharField(max_length=50, blank=True)
 	region = models.CharField(max_length=50, blank=True)
 	city = models.CharField(max_length=50, blank=True)
+	postal_code = models.CharField(max_length=50, blank=True)
+	neighborhood = models.CharField(max_length=50, blank=True)
+	address = models.CharField(max_length=50, blank=True)
 	description = models.TextField(max_length=255, blank=True)
 
 	def save(self, *args, **kwargs):
+		# Get any Available geo data
+		self.country = self.getWorldData('country')
+		self.region = self.getWorldData('region')
+		self.city = self.getWorldData('place')
+		self.postal_code = self.getWorldData('postcode')
+		self.neighborhood = self.getWorldData('neighborhood')
+		self.address = self.getWorldData('address')
 		try:
-			# Get Country, Region and City
-			geocoder = Geocoder(access_token=settings.MAPBOX_ACCESS_TOKEN)
-			response = geocoder.reverse(lon=self.location.x, lat=self.location.y)
-			features = sorted(response.geojson()['features'], key=lambda x: x['place_name'])
-			print(features)
-			#print(features[0]['text'])
-			self.city = features[0]['text']
-			#self.region =
-			#self.country =
-
-			print(features[0]['context'][0])
-			print(features[0]['context'][1])
-			#_features.split(',')
-
-
-			#self.country = WorldBorder.objects.get(mpoly__intersects=self.location).name
+			print(VeredasColombia.objects.get(geom__intersects=self.location).name)
 		except Exception as e:
-			print(e)
+			print("no vereda")
+			pass
+
+		#self.country = WorldBorder.objects.get(mpoly__intersects=self.location).name
 		super(Record, self).save(*args, **kwargs)
+
+	def getWorldData(self, type):
+		try:
+			geocoder = Geocoder(access_token=settings.MAPBOX_ACCESS_TOKEN)
+			response = geocoder.reverse(lon=self.location.x, lat=self.location.y, limit=1, types=[type])
+			features = sorted(response.geojson()['features'], key=lambda x: x['place_name'])
+			return features[0]['text']
+		except Exception as e:
+			pass
+			return ""
+
 
 	def transformToEPSG(self):
 		ct = CoordTransform(SpatialReference(4326), SpatialReference(3857))
