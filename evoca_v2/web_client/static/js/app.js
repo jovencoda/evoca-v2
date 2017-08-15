a084f80cf38e9ff019e28d6d27c790a3f55a5804
jovencoda <genitor06@gmail.com>
2017-08-15 01:05:02 -0500

    Added audio progress and sugar

--- evoca_v2/web_client/static/js/app.js
+++ evoca_v2/web_client/static/js/app.js
@@ -21,10 +21,11 @@ $(document)
    });
 
    function audioPlayers(){
-
+     audio = null;
      players = d3.selectAll(".player").on("click", function(d){
        icon = $(this).children();
        audio = $(this).find("audio")[0];
+       progress = $(audio).parent().parent().find(".progress");
        if(icon.hasClass("play")){
          // Play audio
          toogleIcon(icon, 'play');
@@ -37,12 +38,24 @@ $(document)
        // ON end toggle icon
        audio.onended = function(){
          toogleIcon(icon, 'stop');
+         progress.progress({percent: 0});
        }
      });
+
+     $("audio").each(function(i){
+       this.ontimeupdate = function() {updateProgress(this)};
+     });
+
+     function updateProgress(audio){
+        // Update time
+        progress = $(audio).parent().parent().parent().find(".progress");
+        progress.progress({percent: parseInt((audio.currentTime*100)/audio.duration)});
+     }
 
      function toogleIcon(icon, value){
        if(value=='play'){
          icon.removeClass("play");
+         
          icon.addClass("pause");
        }else{
          icon.removeClass("pause");
