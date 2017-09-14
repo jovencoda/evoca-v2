0395fa2b987b78b04c0db0f5606b7d4782e11b7d
jovencoda <genitor06@gmail.com>
2017-09-14 14:18:57 -0500

    Added token auth to maps

--- provisioning/createsuperuser.py
+++ provisioning/createsuperuser.py
@@ -4,6 +4,26 @@
 import django
 import os
 from django.contrib.auth import get_user_model
-password = 'password' 
-User = get_user_model()
-User.objects.create_superuser('root', 'myemail@test.com', password)
+from rest_framework.authtoken.models import Token
+
+# -------- Create root user --------
+
+try:
+    password = 'password' 
+    User = get_user_model()
+    User.objects.create_superuser('root', 'myemail@test.com', password)
+except Exception as e:
+    print(e)
+    pass
+
+# -------- Create Auth token --------
+
+try:
+    root_user = User.objects.get(username='root')
+    token = Token.objects.create(user=root_user)
+    print token.key
+except Exception as e:
+    print(e)
+    pass
+
+
