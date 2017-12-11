c896fbfeeb53b961711ab60bfeba497de32a0e0f
JuanEstebanLopez <jelg1994@gmail.com>
2017-12-10 21:01:49 -0500

    arreglo lista reportes

--- evoca_v2/mobile/serializers.py
+++ evoca_v2/mobile/serializers.py
@@ -6,7 +6,7 @@ from core.models import *
 class RecordSerializer(serial.ModelSerializer):
 	class Meta:
 		model = Record
-		fields = ('id', 'img_url',	'audio_url', 'descripcion',	'fecha', 'hora', 'latitud',	'longitud')
+		fields = ('pk', 'img_url',	'audio_url', 'descripcion',	'fecha', 'hora', 'latitud',	'longitud')
 
 class imagenSerializer(serial.ModelSerializer):
     class Meta:
