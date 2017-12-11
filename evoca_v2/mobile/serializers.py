e070c3750e85a1c34ff8b04749d1edf601531be3
JuanEstebanLopez <jelg1994@gmail.com>
2017-12-10 21:09:03 -0500

    modificacion url_reportes en serializaers

--- evoca_v2/mobile/serializers.py
+++ evoca_v2/mobile/serializers.py
@@ -4,6 +4,8 @@ from models import *
 from core.models import *
 
 class RecordSerializer(serial.ModelSerializer):
+	img_url = serializers.CharField()
+	audio_url = serializers.CharField()
 	class Meta:
 		model = Record
 		fields = ('pk', 'img_url',	'audio_url', 'descripcion',	'fecha', 'hora', 'latitud',	'longitud')
