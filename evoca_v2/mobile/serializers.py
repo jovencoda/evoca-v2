4bd934c176f892b1d56e5666000cb15190d9ece3
JuanEstebanLopez <jelg1994@gmail.com>
2017-12-12 22:27:45 -0500

    serializer all methods V2.5 (coding utf-8)

--- evoca_v2/mobile/serializers.py
+++ evoca_v2/mobile/serializers.py
@@ -34,9 +34,9 @@ class RecordSerializer(serial.ModelSerializer):
 	def get_hora(self, ob):
 		return ob.created_at
 	def get_latitud(self, ob):
-		return str(obj.getRawLocation()).split(“,”)[0]
+		return str(obj.getRawLocation()).split(",")[0]
 	def get_longitud(self, ob):
-		return str(obj.getRawLocation()).split(“,”)[1]
+		return str(obj.getRawLocation()).split(",")[1]
 
 
 
