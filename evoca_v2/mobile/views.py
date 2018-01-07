b64493a876f84639904be5aeaa383c2bfe591714
JuanEstebanLopez <jelg1994@gmail.com>
2018-01-07 17:14:28 -0500

    modificaciones exempt subir archivos 5

--- evoca_v2/mobile/views.py
+++ evoca_v2/mobile/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from django.utils.decorators import method_decorator
+
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework import status, permissions
