bae1c4a437fd301c6f11bd5752e4ca28ad1c665c
hansTra77 <jupame2006@hotmail.com>
2018-04-22 15:25:19 -0500

    cambios en el admin

--- evoca_v2/mobile/admin.py
+++ evoca_v2/mobile/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from mobile.models import *
 
 # Register your models here.
+admin.site.register(Imagen)
+admin.site.register(Audio)
